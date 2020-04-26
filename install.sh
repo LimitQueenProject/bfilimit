fa8818416aba8a2292f93de46c005a9ec6aca12f
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-26 10:38:55 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+apt update -y
+apt upgrade -y
+pip2 install requests mechanize hashlib colorama bs4 js
+pip install requests mechanize hashlib colorama bs4 js
+apt install tor -y
+apt install perl
+./BF
