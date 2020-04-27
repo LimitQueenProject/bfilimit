1bb4afbf06df67948ec06b474be1a72d0575aa73
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-27 08:58:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 clear
 apt update -y
 apt upgrade -y
-pip2 install requests mechanize hashlib colorama bs4 js
-pip install requests mechanize hashlib colorama bs4 js
+apt install python
+apt install pyhton2
+apt install python3
+pip2 install requests mechanize hashlib colorama bs4 js future
+pip3 install requests mechanize hashlib colorama bs4 js future
+pip install requests mechanize hashlib colorama bs4 js future
 apt install tor -y
 apt install perl
 ./BF
